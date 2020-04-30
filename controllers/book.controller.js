6d2f8aaedf264aa22876abbb6578ca0d647d02f1
Glitch (brief-tender-partner) <none>
2020-04-30 13:31:40 +0000

    🎬🏙 Updated with Glitch

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,55 +1,50 @@
 var db = require('../db');
 var shortid = require('shortid');
 
-module.exports.index = function(req, res){
-    res.render('books/index',{
-        books : db.get('books').value()
-    })
+
+module.exports.index = function(req, res) {
+  res.render("books/index", {
+    books: db.get("books").value()
+  });
 };
 
-module.exports.search = function(req,res){
-    var name = req.query.title;
-    var matchedUser = db.get('books').value().filter(function(book){
-        return book.title.toLowerCase().indexOf(name.toLowerCase()) !== -1
-    });
-    res.render('books/index',{   
-        books : matchedUser
-    })
+module.exports.delete = function(req, res) {
+  var id = req.params.id;
+
+  db.get("books")
+    .remove({ id: id })
+    .write();
+
+  res.render("books/index", {
+    books: db.get("books").value()
+  });
 };
 
-module.exports.create = function(req, res){
-    res.render('books/create')
+module.exports.update = function(req, res) {
+  var id = req.params.id;
+  res.render("books/update-title", {
+    id: id
+  });
 };
 
-module.exports.get = function(req,res){
-    var id = req.params.id;
-    var book = db.get('books').find({id : id}).value()
-    res.render('books/view',{
-        book: book
-    })
+module.exports.postCreate = function(req, res) {
+  
+  req.body.id = shortid.generate();
+  db.get("books")
+    .push(req.body)
+    .write();
+  res.render("books/index", {
+    books: db.get("books").value()
+  });
 };
 
-module.exports.postCreate = function(req, res){
-    req.body.id = shortid.generate();
-    db.get('books').push(req.body).write();
-    res.redirect('/books')
-};
+module.exports.postUpdate = function(req, res) {
+  var id = req.body.id;
+  var title = req.body.title;
+    db.get("books")
+      .find({ id: id })
+      .assign({ title: title })
+      .write();
 
-module.exports.delete = function(req, res){
-    var id_delete = req.params.id;
-    db.get('books').remove({id : id_delete}).write();
-    res.redirect('/books');
-};
-
-module.exports.update = function(req, res){
-    var book = db.get('books').find({id: req.params.id}).value();
-    res.render('books/update',{
-        book : book
-    });
-};
-
-module.exports.postUpdate = function(req, res){
-    var id = req.body.id
-    db.get('books').find({id : id}).assign({title :req.body.title }).write()
-    res.redirect('/books');
+    res.redirect("/books");
 };
