021e433b9490331a9e744f6c99a104d7ac9170d8
hakida <vietlongit@gmail.com>
2020-05-04 00:45:27 +0800

    con 1 bug ma fix mai khong xong buon qua

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -32,5 +32,25 @@ module.exports.delete = function(req, res){
 };
 
 module.exports.create = function(req, res){
-    res.render('users/create');
+    res.render('users/index')
+};
+
+module.exports.postCreate = function(req, res){
+    req.body.id = shortid.generate();
+    db.get('users').push(req.body).write();
+    res.redirect('/users');
+};
+
+module.exports.update = function(req, res){
+    var user = db.get('users').find({ id : parseInt(req.params.id) }).value();
+    res.render('users/update',{
+        user : user
+    });
+};
+
+module.exports.postUpdate =function(req, res){
+    var id = parseInt(req.body.id);
+    db.get('users').find({ id : id }).assign({ name : req.body.name }).write()
+    res.redirect('/users');
+    
 };
--- routers/users.router.js
+++ routers/users.router.js
@@ -3,9 +3,20 @@ var controller = require('../controllers/user.controller');
 var router = express.Router();
 
 router.get('/', controller.index);
+
 router.get('/search', controller.search);
+
 router.get('/:id', controller.get);
+
 router.get('/:id/delete', controller.delete);
+
 router.get('/create', controller.create);
+
+router.post('/create', controller.postCreate);
+
+router.get('/:id/update', controller.update);
+
+router.post('/update', controller.postUpdate);
+
 
 module.exports = router;
