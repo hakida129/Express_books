3d77901496dcd5abcfa1ee48a8d8bd9d0620a45c
hakida <vietlongit@gmail.com>
2020-05-10 12:34:00 +0800

    login middleware

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ var app = express();
 var bookRouter = require('./routers/books.router');
 var userRouter = require('./routers/users.router');
 var transactionRouter = require('./routers/transactions.router');
+var authRouter = require('./routers/auth.router');
 var countCookie = require('./validation/countCookie.middleware');
+var authMiddleware = require('./middlewares/auth.middleware');
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -20,15 +22,15 @@ var port = 3333;
 app.set('view engine', 'pug');
 app.set('views', './views')
 
-app.get('/', countCookie.countCookie , function(req, res){
-    res.cookie('cookie', 1);
+app.get('/', function(req, res){
     res.render('index');
     
 });
 
-app.use('/books', countCookie.countCookie ,  bookRouter);
-app.use('/users', countCookie.countCookie , userRouter);
-app.use('/transactions', countCookie.countCookie , transactionRouter);
+app.use('/books', bookRouter);
+app.use('/users', authMiddleware.requireMiddleware , userRouter);
+app.use('/transactions', transactionRouter);
+app.use('/auth', authRouter);
 
 app.listen(port, function(){
     console.log('Example app listerning at port ' + port);
--- validation/countCookie.middleware.js
+++ validation/countCookie.middleware.js
@@ -1,5 +1,4 @@
 module.exports.countCookie = function(req, res, next){
     res.cookie('cookie', parseInt(req.cookies.cookie) + 1);
-    console.log("Number access website : " + parseInt(req.cookies.cookie) + 1 );
     next();
 };
